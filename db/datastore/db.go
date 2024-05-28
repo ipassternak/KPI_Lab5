8acfe5e5e7c8345e3047307c10db4a486644931d
Ivan Pasternak <retriumpf@gmail.com>
2024-05-28 21:02:53 +0300

    feat: add db file segmentation

--- db/datastore/db.go
+++ db/datastore/db.go
@@ -7,35 +7,52 @@ import (
 	"io"
 	"os"
 	"path/filepath"
+	"strconv"
+	"strings"
+	"time"
 )
-
-const outFileName = "current-data"
 
 var ErrNotFound = fmt.Errorf("record does not exist")
 
-type hashIndex map[string]int64
+const (
+	SEGMENT_EXT = ".seg"
+)
+
+type hashIndex map[string][2]int64
 
 type Db struct {
-	out *os.File
-	outPath string
-	outOffset int64
+	out            *os.File
+	outOffset      int64
+	segmentIndex   int
+	maxSegmentSize int64
+	outDir         string
 
 	index hashIndex
 }
 
-func NewDb(dir string) (*Db, error) {
-	outputPath := filepath.Join(dir, outFileName)
-	f, err := os.OpenFile(outputPath, os.O_APPEND|os.O_WRONLY|os.O_CREATE, 0o600)
+func (db *Db) loadSegment() error {
+	segmentPath := filepath.Join(db.outDir, fmt.Sprintf("%d.db", db.segmentIndex))
+	segment, err := os.OpenFile(segmentPath, os.O_APPEND|os.O_WRONLY|os.O_CREATE, 0o600)
 	if err != nil {
+		return err
+	}
+	db.out = segment
+	db.outOffset = 0
+	return nil
+}
+
+func NewDb(dir string, maxSegmentSize int64) (*Db, error) {
+	db := &Db{
+		index:          make(hashIndex),
+		maxSegmentSize: maxSegmentSize,
+		outDir:         dir,
+	}
+	err := db.recover()
+	if err != nil && err != io.EOF {
 		return nil, err
 	}
-	db := &Db{
-		outPath: outputPath,
-		out:     f,
-		index:   make(hashIndex),
-	}
-	err = db.recover()
-	if err != nil && err != io.EOF {
+	err = db.loadSegment()
+	if err != nil {
 		return nil, err
 	}
 	return db, nil
@@ -43,49 +60,80 @@ func NewDb(dir string) (*Db, error) {
 
 const bufSize = 8192
 
-func (db *Db) recover() error {
-	input, err := os.Open(db.outPath)
+func (db *Db) recoverSegmentIndex() error {
+	files, err := os.ReadDir(db.outDir)
 	if err != nil {
 		return err
 	}
-	defer input.Close()
 
-	var buf [bufSize]byte
-	in := bufio.NewReaderSize(input, bufSize)
-	for err == nil {
-		var (
-			header, data []byte
-			n int
-		)
-		header, err = in.Peek(bufSize)
-		if err == io.EOF {
-			if len(header) == 0 {
+	var segmentIndex int
+
+	for _, file := range files {
+		filename := file.Name()
+		if filepath.Ext(filename) == SEGMENT_EXT {
+			basename := strings.TrimSuffix(filename, SEGMENT_EXT)
+			index, err := strconv.Atoi(basename)
+			if err != nil {
 				return err
 			}
-		} else if err != nil {
-			return err
-		}
-		size := binary.LittleEndian.Uint32(header)
-
-		if size < bufSize {
-			data = buf[:size]
-		} else {
-			data = make([]byte, size)
-		}
-		n, err = in.Read(data)
-
-		if err == nil {
-			if n != int(size) {
-				return fmt.Errorf("corrupted file")
+			if index > segmentIndex {
+				db.segmentIndex = index
 			}
-
-			var e entry
-			e.Decode(data)
-			db.index[e.key] = db.outOffset
-			db.outOffset += int64(n)
 		}
 	}
-	return err
+
+	return nil
+}
+
+func (db *Db) recover() error {
+	err := db.recoverSegmentIndex()
+	if err != nil {
+		return err
+	}
+	for i := 0; i <= db.segmentIndex; i++ {
+		input, err := os.OpenFile(fmt.Sprintf("%d%s", i, SEGMENT_EXT), os.O_RDONLY, 0o600)
+		if err != nil {
+			return err
+		}
+		defer input.Close()
+
+		var buf [bufSize]byte
+		in := bufio.NewReaderSize(input, bufSize)
+		for err == nil {
+			var (
+				header, data []byte
+				n            int
+			)
+			header, err = in.Peek(bufSize)
+			if err == io.EOF {
+				if len(header) == 0 {
+					return err
+				}
+			} else if err != nil {
+				return err
+			}
+			size := binary.LittleEndian.Uint32(header)
+
+			if size < bufSize {
+				data = buf[:size]
+			} else {
+				data = make([]byte, size)
+			}
+			n, err = in.Read(data)
+
+			if err == nil {
+				if n != int(size) {
+					return fmt.Errorf("corrupted file")
+				}
+
+				var e entry
+				e.Decode(data)
+				db.index[e.key] = [2]int64{int64(i), db.outOffset}
+				db.outOffset += int64(n)
+			}
+		}
+	}
+	return nil
 }
 
 func (db *Db) Close() error {
@@ -93,18 +141,21 @@ func (db *Db) Close() error {
 }
 
 func (db *Db) Get(key string) (string, error) {
-	position, ok := db.index[key]
+	segmentInfo, ok := db.index[key]
 	if !ok {
 		return "", ErrNotFound
 	}
 
-	file, err := os.Open(db.outPath)
+	segmentIndex := segmentInfo[0]
+	segmentOffset := segmentInfo[1]
+
+	file, err := os.Open(fmt.Sprintf("%d%s", segmentIndex, SEGMENT_EXT))
 	if err != nil {
 		return "", err
 	}
 	defer file.Close()
 
-	_, err = file.Seek(position, 0)
+	_, err = file.Seek(segmentOffset, 0)
 	if err != nil {
 		return "", err
 	}
@@ -124,8 +175,57 @@ func (db *Db) Put(key, value string) error {
 	}
 	n, err := db.out.Write(e.Encode())
 	if err == nil {
-		db.index[key] = db.outOffset
+		db.index[key] = [2]int64{int64(db.segmentIndex), db.outOffset}
 		db.outOffset += int64(n)
+		if db.outOffset >= db.maxSegmentSize {
+			db.out.Close()
+			db.segmentIndex++
+			err := db.loadSegment()
+			if err != nil {
+				return err
+			}
+		}
 	}
 	return err
+}
+
+func (db *Db) Merge() error {
+	swapFilename := fmt.Sprintf("%d%s", time.Now().Unix(), SEGMENT_EXT)
+	swapFile, err := os.OpenFile(swapFilename, os.O_APPEND|os.O_WRONLY|os.O_CREATE, 0o600)
+	if err != nil {
+		return err
+	}
+	defer func() {
+		swapFile.Close()
+		os.Remove(swapFilename)
+	}()
+	for key := range db.index {
+		value, err := db.Get(key)
+		if err != nil {
+			return err
+		}
+		e := entry{
+			key:   key,
+			value: value,
+		}
+		_, err = swapFile.Write(e.Encode())
+		if err != nil {
+			return err
+		}
+	}
+
+	for i := 0; i <= db.segmentIndex; i++ {
+		segmentPath := filepath.Join(db.outDir, fmt.Sprintf("%d%s", i, SEGMENT_EXT))
+		err := os.Remove(segmentPath)
+		if err != nil {
+			return err
+		}
+	}
+
+	err = os.Rename(swapFilename, fmt.Sprintf("%d%s", 0, SEGMENT_EXT))
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
